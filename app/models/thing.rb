b3880242264109320fb4db7f56cfa2ec63885237
Fladson Gomes <fladsonthiago@gmail.com>
2024-12-21 10:40:19 +0100

    Broadcast votes

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,6 +1,15 @@
 class Thing < ApplicationRecord
   def upvote!
     self.votes += 1
-    save!
+    if save!
+      broadcast_upvote
+    end
   end
+
+  private
+    def broadcast_upvote
+      broadcast_replace_to "votes",
+        target: "votes_thing_#{id}",
+        partial: "things/votes"
+    end
 end
